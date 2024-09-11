7328b3c99d10bc15c15f49cc6ff8622b6bf0bc84
Alexander Goussas <goussasalexander@gmail.com>
2024-09-11 10:12:18 -0500

    feature: Add contacts section to footer

--- src/lib/components/ag-footer.svelte
+++ src/lib/components/ag-footer.svelte
@@ -1,13 +1,27 @@
 <script lang="ts">
-	import { Github, Linkedin, Twitter } from 'lucide-svelte';
+	import { Github, Linkedin, Twitter, Mail } from 'lucide-svelte';
 </script>
 
 <footer
-	class="bg-primary absolute bottom-0 flex h-[80px] w-full items-center justify-center p-1 text-white"
+	class="absolute bottom-0 flex h-[150px] w-full items-center justify-center bg-primary p-1 text-white"
 >
-	<div class="flex gap-4">
-		<a href="https://github.com/aloussase"><Github /></a>
-		<a href="https://linkedin.com/in/alexander-goussas"><Linkedin /></a>
-		<a href="https://x.com/aloussase"><Twitter /></a>
-	</div>
+	<section id="contacts" class="flex w-full justify-around">
+		<div>
+			<h2>Contact</h2>
+			<div class="flex gap-1">
+				<Mail /> <a href="mailto:goussasalexander@gmail.com">goussasalexander@gmail.com</a>
+			</div>
+		</div>
+
+		<div>
+			<h2>Socials</h2>
+			<div id="socials" class="flex flex-col gap-2">
+				<a class="flex gap-1" href="https://github.com/aloussase"><Github /> @aloussase</a>
+				<a class="flex gap-1" href="https://linkedin.com/in/alexander-goussas">
+					<Linkedin /> alexander-goussas
+				</a>
+				<a class="flex gap-1" href="https://x.com/aloussase"><Twitter /> @aloussase</a>
+			</div>
+		</div>
+	</section>
 </footer>
